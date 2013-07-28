ba163c61a53876a2b2d0df0d848a318f4249c4bf
Carl Svensson <csvenss2@gmail.com>
2013-07-28 16:54:17 -0700

    skeleton of rails app checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShinyFinance::Application.config.secret_key_base = 'fb22e876de958f4bae6ecbf821f37cbd89bda1a455f41632a5a27e677ab1358b3e2ae3d83223edf8dac0d5519e1c236a62f076679a74e8ad35aedd6e8dee86bb'
