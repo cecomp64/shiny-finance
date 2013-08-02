020eb7fd97b6cc1c8e83726167c0c73ccc5675d0
Carl Svensson <csvenss2@gmail.com>
2013-08-01 19:41:40 -0700

    Refactor layouts to use header/footer, and refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do
-    it "should have the content 'Shiny'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Shiny')
-    end
+  # Makes "it" the page object in the tests below
+  subject {page}
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('Home')
-    end
+  # Begin tests for home page
+  # This is named purely for readability
+  describe "Home page" do
+
+    # Tests following the vefore statement apply to the root_path page
+    before {visit root_path}
+
+    # Equivalent, but old test
+    #it "should have the content 'Shiny'" do
+    #  expect(page).to have_content('Shiny')
+    #end
+    it { should have_content('Shiny') }
+    it { should  have_title('Shiny') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title('Help')
-    end
+    before {visit help_path}
+    it { should have_content('Help') }
+    it { should have_title('Help') }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title('About')
-    end
+    before {visit about_path}
+    it { should have_content('About') }
+    it { should have_title('About') }
   end
 end
