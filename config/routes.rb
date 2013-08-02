0b79a78e57c69b2ccfa2306ff49635211dfa9673
Carl Svensson <csvenss2@gmail.com>
2013-08-01 19:59:24 -0700

    Adding user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ShinyFinance::Application.routes.draw do
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
 
   resources :transactions
 
