1fb56e6f0c199bd11b252c582e03dfff843da6f5
Carl Svensson <csvenss2@gmail.com>
2013-08-04 18:07:23 -0700

    Started authentication logic, got sidetracked by some design.  Added a site header.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,4 +9,29 @@ describe "AuthenticationPages" do
 		it {should have_content('Sign in')}
 		it {should have_title('Sign in')}
 	end
+
+	describe "signin" do
+		before {visit signin_path}
+
+		describe "with invalid credentials" do
+			before {click_button "Sign in"}
+
+			it {should have_title('Sign in')}
+			it {should have_selector('div.flash.error', text: 'Invalid')}
+		end
+
+		describe "with valid credentials" do
+			let(:user) {FactoryGirl.create(:user)}
+			before do
+				fill_in "Email", with: user.email.upcase
+				fill_in "Password", with: user.password
+			end
+
+			it {should have_title(user.name)}
+			it {should have_link('Profile', href: user_path(user))}
+			it {should have_link('Sign out', href: signout_path)}
+			it {should_not have_link('Sign in', href: signin_path)}
+
+		end
+	end
 end
